410a0fc048968dd903c2042fbd5904336b601089
Nicholas Cutlip <cutlipn@gmail.com>
2024-04-27 14:17:22 -0700

    Add Turtle Graphics constructors to turtleg.cpp

--- turtleg.cpp
+++ turtleg.cpp
@@ -3,3 +3,39 @@
 
 #include "turtleg.h"
 
+/*
+    * TurtleG
+    * Constructor. Construct new TurtleG object
+    * with specified current position (CP) and
+    * current direction (CD).
+    * TurtleG object with current position (CP)
+    * set to (0.0, 0.0)
+    *
+    * @param	x		x coordinate of CP
+    *			y		y coordinate of CP
+    *			angle	initial direction of CD
+    */
+TurtleG(GLfloat x, GLfloat y, GLfloat angle)
+{
+    this->CP = Point2(x, y);
+    this->CD = angle;
+}
+
+/*
+    * TurtleG
+    * Constructor. Construct new TurtleG object
+    * with specified current position (CP) and
+    * current direction (CD).
+    * TurtleG object with current position (CP)
+    * set to (0.0, 0.0)
+    *
+    * @param	x		x coordinate of CP
+    *			y		y coordinate of CP
+    *			angle	initial direction of CD
+    */
+TurtleG(GLfloat x, GLfloat y, GLfloat angle)
+{
+    this->CP = Point2(x, y);
+    this->CD = angle;
+}
+
