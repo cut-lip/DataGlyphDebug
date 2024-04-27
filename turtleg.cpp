4294e9ce1420739abc3762d4182db41995459be2
Nicholas Cutlip <cutlipn@gmail.com>
2024-04-27 14:19:32 -0700

    Add TurtleG public function defs to turtleg.cpp

--- turtleg.cpp
+++ turtleg.cpp
@@ -39,3 +39,88 @@ TurtleG(GLfloat x, GLfloat y, GLfloat angle)
     this->CD = angle;
 }
 
+/*
+    * turnTo
+    * Turn the turtle to given angle from horizontal.
+    * In SPC-SF context, can be considered static angles.
+    *
+    * @param angle Value in degrees to  be turned to
+    */
+void turnTo(GLfloat angle)
+{
+    this->CD = angle;
+}
+
+/*
+    * turn the turtle to given angle relative to current direction
+    * In SPC-SF context, can be considered dynamic angles
+    *
+    * @param angle Value in degrees to be turned to
+    */
+void turn(GLfloat angle)
+{
+    this->CD += angle;
+}
+
+/*
+    * forward
+    * Move the turtle forward a given distance,
+    * drawing a line from start to finish.
+    *
+    * @param	distance	the distance to be traveled
+    *			isVisible	draw the line or not
+    */
+void forward(float distance, bool isVisible)
+{
+    // Determine endpoint based on radial distance
+    float x = CP.getx() + distance * cos(RADIAN_PER_DEGREE * CD);
+    float y = CP.gety() + distance * sin(RADIAN_PER_DEGREE * CD);
+
+    if (isVisible) lineTo(x, y);	// Move CP
+    else moveTo(x, y);
+}
+
+/*
+    * setCP
+    * Set the Turtle's Current Position (CP).
+    *
+    * @param	x	x coordinate of new CP
+    *			y	y coordinate of new CP
+    */
+void setCP(GLfloat x, GLfloat y)
+{
+    this->CP = Point2(x, y);
+}
+
+/*
+    * getCP
+    * Get the Turtle's Current Position (CP).
+    *
+    * @return	Point2	turtle's current position
+    */
+Point2 getCP()
+{
+    return this->CP;
+}
+
+/*
+    * setCD
+    * Set the Turtle's Current Direction (CD).
+    *
+    * @param	angle	direction of the turtle
+    */
+void setCD(GLfloat angle)
+{
+    this->CD = angle;
+}
+
+/*
+    * getCD
+    * Get the Turtle's Current Direction (CD).
+    *
+    * @return	GLfloat	current direction of turtle
+    */
+GLfloat getCD()
+{
+    return this->CD;
+}
--- turtleg.h
+++ turtleg.h
@@ -24,11 +24,7 @@ public:
     * TurtleG object with current position (CP)
     * set to (0.0, 0.0).
     */
-    TurtleG()
-    {
-        this->CP = Point2(0.0, 0.0);
-        this->CD = 0.0;
-    }
+    TurtleG();
 
     /*
     * TurtleG
@@ -42,11 +38,7 @@ public:
     *			y		y coordinate of CP
     *			angle	initial direction of CD
     */
-    TurtleG(GLfloat x, GLfloat y, GLfloat angle)
-    {
-        this->CP = Point2(x, y);
-        this->CD = angle;
-    }
+    TurtleG(GLfloat x, GLfloat y, GLfloat angle);
 
     /*
     * turnTo
@@ -55,10 +47,7 @@ public:
     *
     * @param angle Value in degrees to  be turned to
     */
-    void turnTo(GLfloat angle)
-    {
-        this->CD = angle;
-    }
+    void turnTo(GLfloat angle);
 
     /*
     * turn the turtle to given angle relative to current direction
@@ -66,10 +55,7 @@ public:
     *
     * @param angle Value in degrees to be turned to
     */
-    void turn(GLfloat angle)
-    {
-        this->CD += angle;
-    }
+    void turn(GLfloat angle);
 
     /*
     * forward
@@ -79,15 +65,7 @@ public:
     * @param	distance	the distance to be traveled
     *			isVisible	draw the line or not
     */
-    void forward(float distance, bool isVisible)
-    {
-        // Determine endpoint based on radial distance
-        float x = CP.getx() + distance * cos(RADIAN_PER_DEGREE * CD);
-        float y = CP.gety() + distance * sin(RADIAN_PER_DEGREE * CD);
-
-        if (isVisible) lineTo(x, y);	// Move CP
-        else moveTo(x, y);
-    }
+    void forward(float distance, bool isVisible);
 
     /*
     * setCP
@@ -96,10 +74,7 @@ public:
     * @param	x	x coordinate of new CP
     *			y	y coordinate of new CP
     */
-    void setCP(GLfloat x, GLfloat y)
-    {
-        this->CP = Point2(x, y);
-    }
+    void setCP(GLfloat x, GLfloat y);
 
     /*
     * getCP
@@ -107,10 +82,7 @@ public:
     *
     * @return	Point2	turtle's current position
     */
-    Point2 getCP()
-    {
-        return this->CP;
-    }
+    Point2 getCP();
 
     /*
     * setCD
@@ -118,10 +90,7 @@ public:
     *
     * @param	angle	direction of the turtle
     */
-    void setCD(GLfloat angle)
-    {
-        this->CD = angle;
-    }
+    void setCD(GLfloat angle);
 
     /*
     * getCD
@@ -129,10 +98,7 @@ public:
     *
     * @return	GLfloat	current direction of turtle
     */
-    GLfloat getCD()
-    {
-        return this->CD;
-    }
+    GLfloat getCD();
 
 private:
 
